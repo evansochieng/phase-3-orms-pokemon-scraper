3cd7ccf0625deb159aff6d8d88e5929020610a5d
Evans Ochieng <evansochieng121@gmail.com>
2022-11-04 19:54:12 +0300

    practice creating objects from query results/save record

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,2 +1,14 @@
 -- Write your sql commands to create your db schema here.
 -- It should create a table in a new table in the db/ directory.
+
+--Drop pokemon table if it already exists
+DROP TABLE IF EXISTS pokemon;
+
+
+-- create pokemon table
+-- Define two columns: name and type
+CREATE TABLE IF NOT EXISTS pokemon (
+  id INTEGER PRIMARY KEY, 
+  name TEXT, 
+  type TEXT
+  );
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,36 @@
 class Pokemon
+    #macros
+    attr_accessor :id, :name, :type, :db
+    #initialize instance
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    #save method
+    def self.save(name, type, db)
+        #write insert statement
+        sql = <<-SQL
+          INSERT INTO pokemon (name, type) VALUES (?, ?)
+        SQL
+
+        #execute insert(save) statement - save record to database
+        db.execute(sql, name, type)
+    end
+
+    #find method
+    def self.find(id, db)
+        #write select query statement to return record matching id
+        sql = <<-SQL
+          SELECT * FROM pokemon WHERE id = ?;
+        SQL
+
+        #execute query
+        query_result =db.execute(sql, id)
+
+        #create a POkemon object
+        self.new(id: query_result.first.first, name: query_result.first[1], type: query_result.first.last, db: db)
+    end
 end
